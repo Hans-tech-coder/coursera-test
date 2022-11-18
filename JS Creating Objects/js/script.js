279338e97641745522371976cf57e128b5b36df9
Hans-tech-coder <obenahans@gmail.com>
2022-11-18 13:39:20 +0800

    Object Literal

--- JS Creating Objects/js/script.js
+++ JS Creating Objects/js/script.js
@@ -1,16 +1,28 @@
 // Object creation
-var company = new Object();
-company.name = "Facebook";
+// var company = new Object();
+// company.name = "Facebook";
 
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "Blue";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "Blue";
 
-console.log(company);
-console.log("Company CEO name is: " + company.ceo.firstName);
+// console.log(company);
+// console.log("Company CEO name is: " + company.ceo.firstName);
 
-console.log(company["name"]);
+// console.log(company["name"]);
 
-var stockPropName = "stock of company";
-company[stockPropName] = 220;
-console.log("Stock price is: " + company[stockPropName]);
+// var stockPropName = "stock of company";
+// company[stockPropName] = 220;
+// console.log("Stock price is: " + company[stockPropName]);
+
+// Better waay: object literal
+var facebook = {
+  name: "Facebook",
+  ceo: {
+    firstName: "Mark",
+    favColor: "Blue"
+  },
+  "stock of company": 220
+};
+
+console.log(facebook.ceo.firstName);
